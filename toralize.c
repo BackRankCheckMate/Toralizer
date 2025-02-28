3e7c49142da20ece256765d9705de06400de6cee
BackRankCheckMate <kafle.babu.rhythm@gmail.com>
2025-02-28 12:09:15 +0545

    Added Comments for Better Readability

--- toralize.c
+++ toralize.c
@@ -1,85 +1,131 @@
 /*toralize.c*/
+
 #include "toralize.h"
 
-Req *request(struct sockaddr_in *sock2){
-    Req *req;
+Req *request(struct sockaddr_in *sock2) {
 
-    req = malloc(reqsize);
+Req *req;
 
+req = malloc(reqsize);  // Allocate memory for the SOCKS4 request
 
-    req->vn = 4;
-    req->cd = 1;
-    req->dstport = sock2->sin_port;
-    req->dstip = sock2->sin_addr.s_addr;
-    strncpy(req->userid, USERNAME, 8);
+req->vn = 4;                        // SOCKS4 version
 
-    return req;
+req->cd = 1;                        // CONNECT command
+
+req->dstport = sock2->sin_port;     // Destination port (big-endian)
+
+req->dstip = sock2->sin_addr.s_addr;// Destination IP (big-endian)
+
+strncpy(req->userid, USERNAME, 8);  // Copy username (max 8 characters)
+
+return req;  // Return the pointer to the request structure
+
 }
 
+/* Hijacking the original connect, making our own to redirect network to Tor*/
+
 int connect(int s2, const struct sockaddr *sock2,
-        socklen_t addrlen){
-    int     s;
-    struct sockaddr_in sock;
-    Req *req;
-    Res *res;
-    char buf[ressize];
-    int success;
-    char tmp[512];
-    int (*p)(int , const struct sockaddr*, socklen_t );
 
-    /* predicate - property */
-    p = dlsym(RTLD_NEXT, "connect");//regular connect function
-    s = socket(AF_INET, SOCK_STREAM, 0);//here we initiilaized a new socket
-    if (s < 0) {
-        perror("socket");
-        
-        return -1;
-    }
+socklen_t addrlen){
 
-    sock.sin_family = AF_INET;
-    sock.sin_port = htons(PROXYPORT);//we connected here to the proxy port
-    sock.sin_addr.s_addr = inet_addr(PROXY);
+int     s;
 
-    if (p(s, (struct sockaddr *)&sock, sizeof(sock))){//<- we had connect to the previous connect port to the regular connection 
-        perror("connect");
+struct sockaddr_in sock;
 
-        return -1;
-    };
+Req *req;
 
-    printf("Connected to proxy\n");
-    req = request((struct sockaddr_in *)sock2);
-    write(s, req, reqsize);
+Res *res;
 
-    memset(buf, 0, ressize);
-    if(read(s, buf, ressize) < 1)
-    {
-        perror("read");
-        free(req);
-        close(s);
+char buf[ressize];
 
-        return -1;
-    };
+int success;
 
-    res = (Res *)buf;
-    success = (res->cd == 90);
-    if (!success) {
-        fprintf(stderr, "Unable to traverse "
-            "the proxy, error code: %d\n",
-            res->cd);
+char tmp[512];
 
-            close(s);
-            free(req);
-            return -1;
-    }
+int (*p)(int , const struct sockaddr*, socklen_t );
 
-    printf("successfully connected through the proxy.\n ");
-    dup2(s, s2); 
-    free(req);
+/* predicate - property */
 
-    return 0;
+p = dlsym(RTLD_NEXT, "connect");//regular or original connect function
 
-    /*
-    ./toralize 1.2.3.4 80
-     argv0      argv1  argv2
-    */
+s = socket(AF_INET, SOCK_STREAM, 0);//here we initiilaized a new socket
+
+if (s < 0) {
+
+perror("socket");
+
+return -1;
+
+}
+
+sock.sin_family = AF_INET;
+
+sock.sin_port = htons(PROXYPORT);//we connected here to the proxy port 9050
+
+sock.sin_addr.s_addr = inet_addr(PROXY);//set proxy IP address to localhost
+
+if (p(s, (struct sockaddr *)&sock, sizeof(sock))){  // call original connect() to reach proxy
+
+perror("connect");
+
+return -1;
+
+};
+
+printf("Connected to proxy\n");
+
+req = request((struct sockaddr_in *)sock2);  // Build a SOCKS4 request
+
+write(s, req, reqsize);  // Send request to proxy
+
+memset(buf, 0, ressize);  // Clear buffer
+
+if (read(s, buf, ressize) < 1) {  // Read response from proxy
+
+perror("read");
+
+free(req);
+
+close(s);
+
+return -1;
+
+}
+
+res = (Res *)buf;  // Convert buffer to response struct
+
+success = (res->cd == 90);  // Check if response code is 90 (Success)
+
+if (!success) {
+
+fprintf(stderr, "Unable to traverse the proxy, error code: %d\n", res->cd);
+
+close(s);
+
+free(req);
+
+return -1;
+
+}
+
+/* WE ARE NOW AUTHORIZED TO USE THE PROXY*/
+
+/* Redirect traffic through proxy */
+
+printf("successfully connected through the proxy.\n ");
+
+dup2(s, s2);
+
+free(req);
+
+return 0;
+
+/*
+
+./toralize 1.2.3.4 80
+
+argv0      argv1  argv2
+
+*/
+
 }
--- toralize.h
+++ toralize.h
@@ -1,26 +1,26 @@
 /* toralize.h */
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <dlfcn.h>
+#include <stdio.h>      // Standard Input/Output functions
+#include <stdlib.h>     // Standard Library Functions
+#include <string.h>     // String Manipulation Functions
+#include <unistd.h>     // POSIX API 
+#include <sys/socket.h> // Socket API
+#include <arpa/inet.h>  // Functions for IP address manipulations
+#include <netinet/in.h> // Structures for internet domain addresses
+#include <dlfcn.h>      // Dynamic Linking, used to hijack connect() function
 
 
-#define PROXY       "127.0.0.1"
-#define PROXYPORT   9050 /*this is the auto port for every laptop*/
-#define USERNAME "toraliz"
-#define reqsize sizeof(struct proxy_request)
-#define ressize sizeof(struct proxy_response)
+#define PROXY       "127.0.0.1" // Local TOR Proxy Address
+#define PROXYPORT   9050        // Default SOCKS4 proxy port for TOR
+#define USERNAME "toraliz"      //Username for SOCKS4 authentication
+#define reqsize sizeof(struct proxy_request)  // Size of SOCKS4 request struct
+#define ressize sizeof(struct proxy_response) // Size of SOCKS4 response struct
 
-typedef unsigned char int8;
-typedef unsigned short int int16;
-typedef unsigned int int32;
+typedef unsigned char int8;       // 1-byte 
+typedef unsigned short int int16; // 2-bytes
+typedef unsigned int int32;       // 4-bytes
 
 /*
-
+  SOCKS4 Request Format
 
                 +----+----+----+----+----+----+----+----+----+----+....+----+
                 | VN | CD | DSTPORT |      DSTIP        | USERID       |NULL|
@@ -30,32 +30,38 @@ typedef unsigned int int32;
 */
 
 struct proxy_request {
-    int8 vn;
-    int8 cd;
-    int16 dstport;
-    int32 dstip;
-    unsigned char userid[8];
+  int8 vn;           // SOCKS version number (4 for SOCKS4)
+  int8 cd;           // Command code (1 = CONNECT, 2 = BIND)
+  int16 dstport;     // Destination port (big-endian format)
+  int32 dstip;       // Destination IP address (big-endian format)
+  unsigned char userid[8];  // User ID (SOCKS4 requires a username)
 };
+typedef struct proxy_request Req; // Define an alias for easy reference
 
-typedef struct proxy_request Req;
+
 /*
+  SOCKS4 Response Format
                 +----+----+----+----+----+----+----+----+
                 | VN | CD | DSTPORT |      DSTIP        |
                 +----+----+----+----+----+----+----+----+
  # of bytes:	   1    1      2              4
 */
 
+
 struct proxy_response {
-    int8 vn;
-    int8 cd;
-    int16 _;
-    int32 __;
+  int8 vn;  // Response version (always 0 in SOCKS4)
+  int8 cd;  // Response status code (90 = success, 91 = failure)
+  int16 _;  // Unused (padding)
+  int32 __; // Unused (padding)
 };
-typedef struct proxy_response Res;
+typedef struct proxy_response Res; // Alias for easy reference
+
 
 Req *request(struct sockaddr_in *);
 int connect (int , const struct sockaddr*, socklen_t );
 /*
+  Purpose of this Header File
+
   1. TURN THE CLIENT INTO A LIB(SHARED LIBRARY) .SO
   2. TURN MAIN() INTO OUR OWN CONNECT()
   3. REPLACE REGULAR CONNECT()
